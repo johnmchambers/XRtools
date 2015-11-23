8ba590bb6a065148599ae18ad0edfd99a78f1d84
John M. Chambers <jmc@stat.stanford.edu>
2015-11-23 11:08:24 -0800

    move pointerString to XRcppExamples

--- R/pointerString.R
+++
@@ -1,8 +0,0 @@
-#' Character String for the Pointer to an R object
-#'
-#' Returns a character string containing the hexadecimal form of
-#' the \dQuote{address} of an arbitrary R object.
-#'@param x Any R object
-#'@template authorKeyword
-pointerString <- function(x)
-    .Call(pointer_string, x)
