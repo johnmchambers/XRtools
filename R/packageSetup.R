d118cbf66da06fcaf9efdf4598d51505d6b12b0d
John M. Chambers <jmc@stat.stanford.edu>
2015-12-02 14:31:48 -0800

    changes to packageSetup()

--- R/packageSetup.R
+++ R/packageSetup.R
@@ -1,18 +1,44 @@
+#' Execute a Setup Step for a Package
+#'
+#' The R code in one or more files is evaluated to carry out setup computations that will
+#' write some code, documentation or anything else into the source directory of an R package.
+#' Designed for package authors who want to use techniques such as the proxy classes in the
+#' XR set of interfaces, \code{compileAttributes()} in Rcpp or inline documentation in roxygen2.
+#'
+#' The computations will be carried out in an environment constructed by\code{packageSetup()}
+#' with the namespace of the package as its parent, and a \code{.packageName} object to
+#' associate it with the package.  By default, the setup step looks for the \code{DESCRIPTION} file in the
+#' working directory to find the package name.  The package must have been installed in the
+#' library path of this R session.
+#'
+#' @param files The names of the files to be parsed and evaluated for the setup step.
+#' @param dir Optional directory to use as the working directory for the evaluation. By default
+#' the current working directory should be the source directory for the package.
+#' @param needPackage The package for which the setup is intended.  Not needed if the working
+#' directory (either currently or given by \code{dir} is the source directory for that package.
+#' If supplied can be either the package name or \code{FALSE} if the setup is not intended for
+#' a package.
 packageSetup <- function(files = "tools/setup.R", dir = ".", needPackage = TRUE) {
     wd <- getwd()
     optT <- getOption("topLevelEnvironment")
     on.exit({setwd(wd); options(topLevelEnvironment = optT)})
     if(!missing(dir))
         setwd(dir)
-    ## check that this is a package source directory
-    if(!file.exists("DESCRIPTION"))
-        stop(gettextf("directory (%s) should be a package source directory, but no DESCRIPTION file",
-                      dQuote(getwd())))
-    if(needPackage) {
-        package <- read.dcf("DESCRIPTION")[,"Package"][[1]]
+    if(identical(needPackage, FALSE))
+        env <- parent.frame()
+    else {
+        if(is.character(needPackage))
+            package <- needPackage
+        ## check that this is a package source directory
+        else {
+            if(!file.exists("DESCRIPTION"))
+                stop(gettextf("directory (%s) should be a package source directory, but no DESCRIPTION file",
+                          dQuote(getwd())))
+            package <- read.dcf("DESCRIPTION")[,"Package"][[1]]
+        }
         env <- new.env(parent = asNamespace(package))
+        assign(".packageName", package, envir = env)
     }
-    else env <- parent.frame()
     options(topLevelEnvironment = env) # metadata for classes, methods, goes here
     for(file in files)
         eval(parse(file),env)
